783aef619efe84bddbeda278c39d76d12e398711
huang1993hao <964353126@qq.com>
2019-01-18 05:27:00 -0800

    huang

---
+++ swiper/config.py
@@ -0,0 +1,24 @@
+
+#
+'''
+{
+ "sid":"39467b989d087c2d92c6132184a365d8",
+ "token":"23f757bad208226ec301e117e40006ed",
+ "appid":"2d92c6132139467b989d087c84a365d8",
+ "templateid":"154501",
+ "param":"87828,3",
+ "mobile":"18011984299",
+ "uid":"2d92c6132139467b989d087c84a365d8"
+}
+'''
+
+# 云之迅的短信配置
+YZX_SMS_API = "https://open.ucpaas.com/ol/sms/sendsms"
+YZX_SMS_PARAMS = {
+    "sid":"44ccbf1a9381af899d233feba3b2529b",
+    "token":"483cbc7005f55bd9530d",
+    "appid": "d804997cda02489ca79a0c289038e636",
+    "templateid":"423817",
+    "param": "",  # 验证码
+    "mobile": "",  # 手机号
+}
